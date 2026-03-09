0f9bfedb8a5dd0357777e9ae49c7163321892754
wake-up-studio <wakeuphuis@gmail.com>
2026-03-09 10:39:06 +0000

    classes

--- The_League/models/Player.php
+++ The_League/models/Player.php
@@ -1,5 +1,48 @@
 <?php
     class Player {
-        
+        public function __construct(private string $nickname, private string $bio, private ? int $id = NULL, private ? int $portrait = NULL, private ? int $team = NULL)
+        {
+            
+        }
+        public function getId():int 
+        {
+            return $this->id;
+        }
+        public function setId(int $id):void
+        {
+            $this->id = $id;
+        }
+        public function getNickname():string
+        {
+            return $this->nickname;
+        }
+        public function setNickname(string $nickname):void
+        {
+            $this->nickname = $nickname;
+        }
+        public function getBio():string
+        {
+            return $this->bio;
+        }
+        public function setBio(string $bio):void
+        {
+            $this->bio = $bio;
+        }
+        public function getPortrait():int
+        {
+            return $this->portrait;
+        }
+        public function setPortrait(int $portrait):void
+        {
+            $this->portrait = $portrait;
+        }
+        public function getTeam():int
+        {
+            return $this->team;
+        }
+        public function setTeam(int $team):void
+        {
+            $this->team = $team;
+        }
     }
 ?>
