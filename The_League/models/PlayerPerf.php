44a6931e1916b621c463ef217cf64b3ae278c06d
smimoon <simonlaroche@orange.fr>
2026-03-12 13:45:35 +0000

    Adding class PlayerPerf and manager PleyrPerfManager

---
+++ The_League/models/PlayerPerf.php
@@ -0,0 +1,44 @@
+<?php
+    class PlayerPerformance {
+        public function __construct(private Player $player, private Game $game, private int $points, private int $assits, private ? int $id)
+        {
+            
+        }
+        
+        public function getPlayer():Player
+        {
+            return $this->player;
+        }
+        public function setPlayer(Player $player):void
+        {
+            $this->player = $player;
+        }
+        
+        public function getGame():Game
+        {
+            return $this->game;
+        }
+        public function setGame(Game $game):void
+        {
+            $this->game = $game;
+        }
+        
+        public function getPoints():int
+        {
+            return $this->points;
+        }
+        public function setPoints(int $points):void
+        {
+            $this->points = $points;
+        }
+        
+        public function getId():int
+        {
+            return $this->id;
+        }
+        public function setId(int $id):void
+        {
+            $this->id = $id;
+        }
+        
+    }
